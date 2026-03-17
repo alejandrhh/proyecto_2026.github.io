c13d735bb175c4534b6a81062f1374a7e103c033
RosarioRaymundo <raymundorosario029@gmail.com>
2026-03-16 21:46:45 -0600

    actualizar app para render

--- app.py
+++ app.py
@@ -130,4 +130,8 @@ def eliminar(id):
     return redirect("/")
 
 
-app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
